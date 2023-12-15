7ab3e720834a99587ecad79f7e1f067da9116105
Manu <manugeo13@gmail.com>
2023-12-15 11:48:37 +0530

    Removed the unused rates endpoint.

--- utils/config.js
+++ utils/config.js
@@ -2,9 +2,12 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT || 3001
-const MONGODB_URI = process.env.NODE_ENV === 'test'
-  ? process.env.TEST_MONGODB_URI
-  : process.env.MONGODB_URI
+
+const MONGODB_URI = (process.env.NODE_ENV === 'production')
+  ? process.env.MONGODB_URI
+  : (process.env.NODE_ENV === 'test')
+    ? process.env.TEST_MONGODB_URI
+    : process.env.DEV_MONGODB_URI
 
 module.exports = {
   PORT,
